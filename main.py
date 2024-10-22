87facc12fcb24efa1c2843d321f4e100265b1b93
PluNo76 <tqnbui@gmail.com>
2024-10-22 23:16:46 +0700

    Đã làm tới phút thứ 32:10 video https://www.youtube.com/watch?v=C6YLJUz9Eoc&ab_channel=C%C3%B4ngNgh%E1%BB%87H%C3%A0ngNg%C3%A0y

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ image_path = 'imgs/peo1.jpg'
 face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
 
 img = cv2.imread(image_path)
-print(img)
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 
@@ -19,9 +18,12 @@ while True:
 
 
     faces = face_detector.detectMultiScale(img_gray, 1.3, 5)
+    count = 0
     for (x, y, w, h) in faces:
-
+        img_face = cv2.resize(img[y + 3: y + h - 3, x + 3: x + w - 3], (64,64))
+        cv2.imwrite('imgs/peo1_{}.jpg'.format(count), img_face)
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        count += 1
 
     cv2.imshow('FRAME', img)
 
