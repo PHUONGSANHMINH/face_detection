667b8d2425e14247c39a90ba2543dceabdfebcce
PluNo76 <tqnbui@gmail.com>
2024-10-22 23:01:51 +0700

    mới thêm thư viện

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ image_path = 'imgs/peo1.jpg'
 face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
 
 img = cv2.imread(image_path)
+print(img)
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 
@@ -19,6 +20,7 @@ while True:
 
     faces = face_detector.detectMultiScale(img_gray, 1.3, 5)
     for (x, y, w, h) in faces:
+
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     cv2.imshow('FRAME', img)
