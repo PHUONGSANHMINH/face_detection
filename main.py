5a736cf4e6921386144cb991bbc5e69bd4e2f4ba
damtrungnguyen <damtrungnguyen2k4@gmail.com>
2024-10-23 23:14:23 +0700

    xong 1 video 1

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
 #Detech face of images
 import cv2
 from cv2.data import haarcascades
-
-image_path = 'imgs/peo1.jpg'
-
-
+import os
 
 face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
 
-img = cv2.imread(image_path)
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-
-
-
-while True:
-
+def getFaces(image_path):
+    img = cv2.imread(image_path)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     faces = face_detector.detectMultiScale(img_gray, 1.3, 5)
     count = 0
     for (x, y, w, h) in faces:
-        img_face = cv2.resize(img[y + 3: y + h - 3, x + 3: x + w - 3], (64,64))
-        cv2.imwrite('imgs/peo1_{}.jpg'.format(count), img_face)
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        img_face = cv2.resize(img[y + 3: y + h - 3, x + 3: x + w - 3], (64, 64))
+        cv2.imwrite(img_path.replace('imgs','imgs_face').split('.jpg')[0] + '_{}.jpg'.format(count), img_face)
+        # cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
         count += 1
 
-    cv2.imshow('FRAME', img)
+image_path = 'imgs'
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+for whatelse in os.listdir(image_path):
+    whatelse_path = os.path.join(image_path, whatelse)
+    for sub_whatelse in os.listdir(whatelse_path):
+        img_path = os.path.join(whatelse_path, sub_whatelse)
+        if not os.path.isdir(whatelse_path.replace('imgs', 'imgs_face')):
+            os.mkdir(whatelse_path.replace('imgs', 'imgs_face'))
+            # print(whatelse_path.replace('imgs', 'imgs_face'))
+        if img_path.endswith('.jpg'):
+            getFaces(img_path)
+
+
+
+
+
+
+
+
 
 
 cv2.destroyAllWindows()
